535311a78267b90bb05e7048a8e16e7d28c6e1fe
Akmal-Faiz <akmal.faiz756@gmail.com>
2020-05-22 14:51:38 +0800

    Update diabetes_prediction_train.py

--- diabetes_prediction_train.py
+++ diabetes_prediction_train.py
@@ -12,29 +12,17 @@ import json
 import numpy as np
 import pandas as pd
 
-from sklearn.svm import SVC
+from sklearn.compose import ColumnTransformer
 from sklearn.externals import joblib
-from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import Binarizer, StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 import sklearn.metrics as metrics
+from sklearn.svm import SVC
+
 from sagemaker_containers.beta.framework import (
     content_types, encoders, env, modules, transformer, worker)
-
-colnames =[
-    'MemberID',
-    'Gender',
-    'Age',
-    'BMI',
-    'hasDiabetes',
-    'avgMorningGlucose',
-    'avgPreMealGlucose',
-    'avgPostMealGlucose',
-    'mostRecentFastingGlucose',
-    'avgSteps',
-    'avgCalories',
-    'avgExercise'
-]
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -45,7 +33,7 @@ if __name__ == '__main__':
     # Sagemaker specific arguments. Defaults are set in the environment variables.
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    parser.add_argument('--train', type=str)#, default=os.environ['SM_CHANNEL_TRAIN'])
 
     args = parser.parse_args()
 
@@ -56,44 +44,33 @@ if __name__ == '__main__':
                           'This usually indicates that the channel ({}) was incorrectly specified,\n' +
                           'the data specification in S3 was incorrectly specified or the role specified\n' +
                           'does not have permission to access the data.').format(args.train, "train"))
+    
     raw_data = [ pd.read_csv(file, engine="python") for file in input_files ]
     data = pd.concat(raw_data)
-    #import pdb; pdb.set_trace()
-    data.drop('MemberID', axis = 1, inplace = True)
-    data.fillna(0, inplace = True)
 
-    # separate labels from data
-    X = data.drop('risk', axis = 1)
+    X = data.drop(['risk'], axis = 1)
     y = data['risk']
 
-    # standardize trg data
-    X = StandardScaler().fit_transform(X)
-
-    # split into trg set and test set
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    # Here we support a single hyperparameter, 'max_leaf_nodes'. Note that you can add as many
-    # as your training my require in the ArgumentParser above.
-    # max_leaf_nodes = args.max_leaf_nodes
-
-    # Now use scikit-learn's decision tree classifier to train the model.
-    clf = SVC()
-    clf = clf.fit(X_train, y_train)
+    numeric_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='constant', fill_value=0)),
+        ('scaler', StandardScaler())])
     
-    # print metrics
-    trg_accuracy = metrics.accuracy_score(y_train, clf.predict(X_train))
-    #trg_average_precision_score = metrics.average_precision_score(y_train, clf.predict(X_train))
-    test_accuracy = metrics.accuracy_score(y_test, clf.predict(X_test))
-    #test_average_precision_score = metrics.accuracy_score(y_test, clf.predict(X_test))
 
-    print('trg_accuracy:',trg_accuracy)
-    #print('trg_average_precision_score:',trg_average_precision_score)
-    print('test_accuracy:',test_accuracy)
-    #print('test_average_precision_score:',test_average_precision_score)
+    preprocessor = ColumnTransformer(
+       transformers = [('tx', numeric_transformer, list(X.columns)[1:])],
+       remainder = 'drop')
+    
+    svc = SVC()
+    
+    final_pipeline = Pipeline(
+        steps = [('preprocessor', preprocessor), ('svc', svc)]
+    )
+    final_pipeline.fit(X_train,y_train)
+    
 
-    # Print the coefficients of the trained classifier, and save the coefficients
-    joblib.dump(clf, os.path.join(args.model_dir, "model.joblib"))
-
+    joblib.dump(final_pipeline, os.path.join(args.model_dir, "model.joblib"))
 
 def input_fn(input_data, content_type):
     """Parse input data payload
@@ -105,7 +82,6 @@ def input_fn(input_data, content_type):
     if content_type == 'text/csv':
         # Read the raw input data as CSV.
         df = pd.read_csv(StringIO(input_data))
-        #df.columns = colnames
         return df
     else:
         raise ValueError("{} not supported by script!".format(content_type))
@@ -119,14 +95,14 @@ def model_fn(model_dir):
     """
     preprocessor = joblib.load(os.path.join(model_dir, "model.joblib"))
     return preprocessor
-'''
+
 def predict_fn(input_data, model):
     """Preprocess input data
     We implement this because the default predict_fn uses .predict(), but our model is a preprocessor
     so we want to use .transform().
     """
-    prediction = model.predict(input_data)
-    return prediction
+    features = model.predict(input_data)
+    return features
 
 def output_fn(prediction, accept):
     """Format prediction output
@@ -146,4 +122,3 @@ def output_fn(prediction, accept):
         return worker.Response(encoders.encode(prediction, accept), accept, mimetype=accept)
     else:
         raise RuntimeException("{} accept type is not supported by this script.".format(accept))
-'''
